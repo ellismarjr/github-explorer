bdf6d6d713b646037481feab119041c3bf1c86d1
Junior de Oliveira <ellismarjr@hotmail.com>
2021-03-11 08:55:23 -0300

    added styles and list repositories from github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,24 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/ellismarjr/repos").then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map((repo) => (
+          <RepositoryItem key={repo.id} repository={repo} />
+        ))}
       </ul>
     </section>
   );
